2bb3f4ca18d6d58e2815dcfc5d3858d03ac965f0
Kong Usa <kongusa650@gmail.com>
2025-05-21 20:02:29 +0700

    this is new commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:git/home_screen.dart';
 
 void main() {
+  // this is the entry point
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
